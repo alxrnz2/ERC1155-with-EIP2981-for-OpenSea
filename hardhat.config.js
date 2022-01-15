5b92dfd2b5f6d115170ad98a14b119f50be59d2d
Alex Rienzie <36116381+alxrnz2@users.noreply.github.com>
2022-01-15 13:42:35 -0700

    Add files via upload

---
+++ hardhat.config.js
@@ -0,0 +1,37 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.2",
+
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+    },
+    mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com"
+    },
+    polygon: {
+      url: "https://polygon-mainnet.infura.io/v3/<ADD API KEY>",
+      accounts: ["<ADD WALLET PRIVATE KEY>"]
+    },
+    rinkeby: {
+      url: "https://rinkeby.infura.io/v3/<ADD API KEY>",
+      accounts: ["<ADD WALLET PRIVATE KEY>"]
+    },
+    ethereum: {
+      url: "https://mainnet.infura.io/v3/<ADD API KEY>",
+      accounts: ["<ADD WALLET PRIVATE KEY>"]
+    }
+  },
+
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: "<ADD API KEY>"
+  },
+};
